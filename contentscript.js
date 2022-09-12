cf17ef0784ea16211f4079cefada0b8656756785
blakewoo <blakewoo0819@gmail.com>
2022-09-12 21:53:13 +0900

    URL Access Prevention page update
    -Creating a function that erases the page of the target URL and draws a new one

--- contentscript.js
+++ contentscript.js
@@ -5,27 +5,23 @@
     // on/off 설정 넣을것
     if(onOffFlag) {
         if(alertFlag === "alert") {
-            chrome.runtime.sendMessage({ action: "popup check" },async function () {
-                let target = location.host
-                let filter = (await findAll(["urls"])).urls
-                for(let i=0;i<filter.length ;i++) {
-                    if(target === filter[i]) {
-
-                        chrome.tabs.query({active: true}, function(tabs){
-                            chrome.action.getPopup(tabs[0].id,function(){
-                                
-                            })
-                        })
-
-                        // 순수 리다이렉트
-                        location.href = "https://google.com"
-                        // 알림 이후 리다이렉트
-
-
-                        break;
-                    }
+            let target = location.host
+            let filter = (await findAll(["urls"])).urls
+            for(let i=0;i<filter.length ;i++) {
+                if(target === filter[i]) {
+                    chrome.scripting.executeScript(
+                        {
+                            target: {tabId: tabId},
+                            func: deleteAllPage
+                        },
+                        () => {});
+                    break;
                 }
-            })
+            }
+        }
+        else {
+            // 순수 리다이렉트
+            location.href = "https://google.com"
         }
     }
 
@@ -35,4 +31,30 @@ async function findAll(itemList) {
     return new Promise(function (resolve, reject) {
         chrome.storage.local.get(itemList, function (data) { resolve(data); })
     })
+}
+
+function deleteAllPage() {
+    let insertionHtml = "<div id=\"contents_div\" style=\"display: none\">\n" +
+        "     <div class=\"alarm-icon\">\n" +
+        "         <img class=\"shark-icon\" src=\"img/icon.png\">\n" +
+        "     </div>\n" +
+        "\n" +
+        "     <div class=\"font-background\">\n" +
+        "         <h1>\n" +
+        "             Attention!\n" +
+        "         </h1>\n" +
+        "         <h2>\n" +
+        "             You really want to enter this website?\n" +
+        "         </h2>\n" +
+        "         <span>\n" +
+        "         <input class=\"public-button\" id=\"yes_button\" type=\"button\" value=\"Of course! I will trash my plan\">\n" +
+        "         <input class=\"public-button marginTop_8px\" id=\"no_button\" type=\"button\" value=\"No, It just mistake\">\n" +
+        "     </span>\n" +
+        "     </div>\n" +
+        "\n" +
+        "     <br>\n" +
+        "     <a style=\"font-size:4pt\" href=\"https://www.flaticon.com/kr/free-icons/\" title=\"상어 아이콘\">상어 아이콘  제작자: Freepik - Flaticon</a>\n" +
+        " </div>"
+        let parent = document.getElementsByTagName("body");
+        parent.innerHtml += insertionHtml;
 }
